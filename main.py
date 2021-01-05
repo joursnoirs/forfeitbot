a69847d4d7bade7aabe9783cd82538ef1265b390
Max Thomas <undeadmax1313@gmail.com>
2021-01-05 20:18:39 +0000

    made bug log, added  command

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
-import os
+import os 
 import requests
 import json
 import praw
 import discord
 import random
+from random import randint 
 from discord.ext import commands
 
+
+#importing the redditconfig 
+from redditsubconfig import REDDIT_APP_SECRET
+from redditsubconfig import REDDIT_APP_ID
+from redditsubconfig import REDDIT_MEME_SUBS
+
 #boot message
-
-
 class MyClient(discord.Client):
     async def on_ready(self):
         print('Logged on as', self.user)
@@ -17,25 +22,20 @@ class MyClient(discord.Client):
 bot = commands.Bot(command_prefix="$")
 
 reddit = praw.Reddit(
-    client_id=(os.getenv('CLIENT_ID')),
-    client_secret=(os.getenv('CLIENT_SECRET')),
+    client_id=REDDIT_APP_ID,
+    client_secret=REDDIT_APP_SECRET,
     user_agent='ForfeitBot made by Mæx#1313')
 
-#class Images(commands.Cog):
-#	def _init_(self, bot):
-#		self.bot= bot
-#		self.reddit = None
-#		if (os.getenv(CLIENT_ID) and (os.getenv(CLIENT_SECRET):
-#			self.reddit = praw.Reddit(
-#   		client_id=(os.getenv('CLIENT_ID')),
-#    		client_secret=(os.getenv('CLIENT_SECRET')),
-#    		user_agent='ForfeitBot made by Mæx#1313')
+
 
 
 #Letting dev know bot is online
 @bot.event
 async def on_ready():
     print('Logged in as {0.user}'.format(bot))
+
+
+
 
 
 #Gives the Ping of the bot. Reminder to add uptime command
@@ -48,7 +48,7 @@ async def ping(ctx):
 #Simple Hello command
 @bot.command()
 async def hello(ctx):
-    await ctx.send('Hello!')
+    await ctx.send("Hello!")
 
 
 #$inspire function as a test for api pulls
@@ -62,113 +62,80 @@ async def inspire(ctx):
 
 @bot.command()
 async def inspite(ctx):
-    await ctx.send('Learn to fucking spell dumbass! its $inspire')
+    await ctx.send('Learn to fucking spell dumbass! its $inspire!')
 
 
-#list of no us
-@bot.command()
-async def whore(ctx):
-    await ctx.send('No u')
-
-
-@bot.command()
-async def bitch(ctx):
-    await ctx.send('No u')
-
-
+#requested by Dalton
 @bot.command()
 async def slut(ctx):
-    await ctx.send('No u')
+    await ctx.send('***YES DADDY!!! HARDER!!!!!***')
 
-
-@bot.command()
-async def idiot(ctx):
-    await ctx.send('No u')
-
-
-@bot.command()
-async def retard(ctx):
-    await ctx.send('No u')
-
-
-@bot.command()
-async def motherfucker(ctx):
-    await ctx.send('No u')
-
-
-@bot.command()
-async def cunt(ctx):
-    await ctx.send('No u')
-
-
-@bot.command()
-async def cumslut(ctx):
-    await ctx.send('No u')
-
-
-@bot.command()
-async def puta(ctx):
-    await ctx.send('No u')
-
-
-@bot.command()
-async def fag(ctx):
-    await ctx.send('No u')\
-   @bot.command()
-
-
-async def faggot(ctx):
-    await ctx.send('No u')
-
-
-@bot.command()
-async def saladtosser(ctx):
-    await ctx.send('No u')
-
-
-@bot.command()
-async def fucker(ctx):
-    await ctx.send('No u')
-
-
-@bot.command()
-async def cocksucker(ctx):
-    await ctx.send('No u')
 
 
 #########################Ideas for bot###############################
 #List maker
-#Sauce supplier
-#make reddit iamge puller
+#Sauce supplier with random numbers
+#make reddit image puller
 #make command to pull copypasta
+#Make a $toxic command that send user to toxicjail.
+
 @bot.command()
 async def uwu(ctx):
-    await ctx.send(
-        'Rawr X3 *nuzzles* How are you? *pounces on you* youre so warm o3o *notices you have a bulge* someones happy! *nuzzles your necky wecky* ~murr~ hehe ;) *rubbies your bulgy wolgy* youre so big! *rubbies more on your bulgy wolgy* it doesnt stop growing .///. *kisses you and licks your neck* daddy likes ;) *nuzzle wuzzle* I hope daddy likes *wiggles butt and squirms* I wanna see your big daddy meat! *wiggles butt* I have a little itch o3o *wags tails* can you please get my itch? *put paws on your chest* nyea~ its a seven inch itch *rubs your chest* can you pwease? *squirms* pwetty pwease? :( I need to be punished *runs paws down your chest and bites lip* like, I need to be punished really good *paws on your bulge as I lick my lips* Im getting thirsty. I could go for some milk *unbuttons your pants as my eyes glow* you smell so musky ;) *licks shaft* mmmmmmmmmmmmmmmmmmm so musky ;) *drools all over your cawk* your daddy meat. I like. Mister fuzzy balls. *puts snout on balls and inhales deeply* oh my gawd. Im so hard *rubbies your bulgy wolgy* *licks balls* punish me daddy nyea~ *squirms more and wiggles butt* I9/11 lovewas an yourinside muskyjob goodness *bites lip* please punish me *licks lips* nyea~ *suckles on your tip* so good *licks precum off your cock* salty goodness~ *eyes roll back and goes balls deep*'
-    )
+	async with ctx.channel.typing():
+  		await ctx.send(
+        	'Rawr X3 *nuzzles* How are you? *pounces on you* youre so warm o3o *notices you have a bulge* someones happy! *nuzzles your necky wecky* ~murr~ hehe ;) *rubbies your bulgy wolgy* youre so big! *rubbies more on your bulgy wolgy* it doesnt stop growing .///. *kisses you and licks your neck* daddy likes ;) *nuzzle wuzzle* I hope daddy likes *wiggles butt and squirms* I wanna see your big daddy meat! *wiggles butt* I have a little itch o3o *wags tails* can you please get my itch? *put paws on your chest* nyea~ its a seven inch itch *rubs your chest* can you pwease? *squirms* pwetty pwease? :( I need to be punished *runs paws down your chest and bites lip* like, I need to be punished really good *paws on your bulge as I lick my lips* Im getting thirsty. I could go for some milk *unbuttons your pants as my eyes glow* you smell so musky ;) *licks shaft* mmmmmmmmmmmmmmmmmmm so musky ;) *drools all over your cawk* your daddy meat. I like. Mister fuzzy balls. *puts snout on balls and inhales deeply* oh my gawd. Im so hard *rubbies your bulgy wolgy* *licks balls* punish me daddy nyea~ *squirms more and wiggles butt* I9/11 lovewas an yourinside muskyjob goodness *bites lip* please punish me *licks lips* nyea~ *suckles on your tip* so good *licks precum off your cock* salty goodness~ *eyes roll back and goes balls deep*' )
+
+
 
 
 @bot.command()
-async def meme(ctx):
-    subreddit = reddit.subreddit("dankmemes")
-    all_subs = []
-    top = subreddit.hot(limit=50)
-    for submission in top:
-        all_subs.append(submission)
-    random_sub = random.choice(all_subs)
-    name = random_sub.title
-    url = random_sub.url
-    em = discord.Embed(title=name)
-    em.set_image(url=url)
-    await ctx.send(embed=em)
+async def meme(ctx,  subreddit: str = ""):
+		async with ctx.channel.typing():
+				reddit = praw.Reddit(
+				client_id=REDDIT_APP_ID,
+				client_secret=REDDIT_APP_SECRET,
+				user_agent="ForfeitBot by Mæx#1313 v1.0")
+				
+				sub_to_pick = REDDIT_MEME_SUBS[random.randint(1,17)]
+				
+				limit=randint(3, 25)
+				post_to_pick = reddit.subreddit(sub_to_pick).hot(limit=limit)
+				await ctx.send('This post came from ' + ((post_to_pick).url))
+				await ctx.send(next(post_to_pick).url)
+	
+							###Might allow to not pick sticky##
+			#post_to_pick = next(x for x in reddit.subreddit(sub_to_pick).hot() if not x.stickied)
 
+
+				#if reddit:
+				#	print('banana')
+						#working
+				#	chosen_subreddit = REDDIT_MEME_SUBS[0]
+				#	if subreddit:
+				#				#checks if sub is in approved list
+				#			if subreddit in REDDIT_MEME_SUBS:
+				#					chosen_subreddit = subreddit
+
+				#	submission = reddit.subreddit(chosen_subreddit).hot()
+
+				#	post_to_pick = random.randint(1, 10)
+				#	for i in range(0, post_to_pick):
+					#	submission = next(x for x in submission if not x.stickied)
+					#await ctx.send(submission.url)
+				
+			#	else:
+			#			await ctx.send('The bot has encountered a hiccup. Please contact @Mæx#1313')
 
 @bot.command()
 async def daddy(ctx):
-    await ctx.send(
-        'Daddy 👨 touches ✋ my 👧 thicc ass 🍑He 👨tells me 👧Im a pretty lass Puts his penis 🍆 in ➡️🍑⬅️ my butt 🤤😫😥Oh my god, Im such a slut! 💃💦Mommy 👩 sees 👀😲 us, and shes mad 😡😠😤Im sorry 😓😔hun, the sex 🍑🍆💦 was bad! 👎🔚Mommy pulls out ⬆️ guns 🔫 akimbo 🔫👩🔫Because she hates 😲😠 her little bimbo 👧The Cummies 💦 are dripping ⬇️💧from my hole 🍑I cant 🙅 stop working 😩😫💪 on Daddys 👨 pole 💈😫Mommy 👩 shoots 🔫, her shots all miss 😂Daddy 👨 and I 👩 share a special ☄️ kiss 💋One ☝️ round left 👈, Mommy 👩 takes the gun 🔫She rests 💤 the barrel 🛢️ on her tongue 👅Goodbye 👋 world 🌍, and goodbye 👋 cheater! 👨I hope youre happy ☺️😄 with your stupid 📖🚫 wife 👩 beater! 👊The gun 🔫 goes bang 💥, Mommy 👩 dies 💀☠️⚰️But we 👨👧 dont 🚫 care, Daddy 👨 just sighs 🌬️😤No 🚫 thoughts 💭 in her 👩 head 💆, no 🚫 blood 💉 in her heart ➡️♥️⬅️That stupid 📖🚫 whore 💃 cant 🚫 tear us apart. 💖💕💓'
-    )
+	async with ctx.channel.typing():
+    
+		await ctx.send('Daddy 👨 touches ✋ my 👧 thicc ass 🍑He 👨tells me 👧Im a pretty lass Puts his penis 🍆 in ➡️🍑⬅️ my butt 🤤😫😥Oh my god, Im such a slut! 💃💦Mommy 👩 sees 👀😲 us, and shes mad 😡😠😤Im sorry 😓😔hun, the sex 🍑🍆💦 was bad!👎🔚Mommy pulls out ⬆️ guns 🔫 akimbo 🔫👩🔫Because she hates 😲😠 her little bimbo 👧The Cummies 💦 are dripping ⬇️💧from my hole 🍑I cant 🙅 stop working 😩😫💪 on Daddys 👨 pole 💈😫Mommy 👩 shoots 🔫, her shots all miss 😂Daddy 👨 and I 👩 share a special ☄️ kiss 💋One ☝️ round left 👈, Mommy 👩 takes the gun 🔫She rests 💤 the barrel 🛢️ on her tongue 👅Goodbye 👋 world 🌍, and goodbye 👋 cheater! 👨I hope youre happy ☺️😄 with your stupid 📖🚫 wife 👩 beater! 👊The gun 🔫 goes bang 💥, Mommy 👩 dies 💀☠️⚰️But we 👨👧 dont 🚫 care, Daddy 👨 just sighs 🌬️😤No 🚫 thoughts 💭 in her 👩 head 💆, no 🚫blood 💉 in her heart➡️♥️⬅️That stupid 📖🚫whore 💃 cant 🚫tear us apart. 💖💕💓')
 
+@bot.command()
+async def homie(ctx):
+	async with ctx.channel.typing():
+		await ctx.send('Yo my homeslice breadslice dawg with butter and salsa my friend, what the hizzle is up with ur dizzle, brodawgio, I dont mean to be unhip, but if you keep this up i may have to get not so dorky with u my friendly amigo of brohominess. It would be most unpleasant and not at all cash money to partake in this kind of un-excellent behavior, but by golly, you seem to leave me with no other option in this fresh turn of happenstance homie-frendo would you like that I would not like to have to get undorky with you, but gosh diddly heckin darn, i find this is a most un-radical, non-groovy, totally unsexy situation I find myself in with you, my bestest of amigofriendlydudes')
 
 @bot.command()
 async def owo(ctx):
---
+++ redditsubconfig.py
@@ -0,0 +1,35 @@
+import os
+
+REDDIT_APP_SECRET = os.getenv("REDDIT_APP_SECRET", False)
+REDDIT_APP_ID = os.getenv("REDDIT_APP_ID", False)
+
+REDDIT_MEME_SUBS = [
+	'memes',
+	'dankmemes',
+	'wtf',
+	'Animemes',
+	'comedycemetary',
+	'AdviceAnimals',
+	'memeeconomy',
+	'prequelmemes',
+	'terriblefacebookmemes',
+	'Cringe',
+	'wokekids',
+	'PewdiepieSubmissions',
+	'funny',
+	'teenagers',
+	'okbuddyretard',
+	'historymemes',
+	'comedyheaven',
+	'WackyTicTacs',
+	'surrealmemes',
+	'nukedmemes',
+	'bonehurtingjuice',
+	'birdswitharms',
+	'nottheonion',
+	'animalswithoutnecks',
+	'Me_irl',
+	'TooMeIrlForMeIrl'
+
+
+]
